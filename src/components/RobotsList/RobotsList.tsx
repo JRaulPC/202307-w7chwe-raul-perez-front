89578d4e940a2419666405ca614f1abe3a3b75a3
Raúl Pérez <jrpcassone@gmail.com>
2023-08-26 20:34:40 +0200

    Add Robots page styles

--- src/components/RobotsList/RobotsList.tsx
+++ src/components/RobotsList/RobotsList.tsx
@@ -1,11 +1,12 @@
 import { useAppSelector } from "../..";
 import RobotCard from "../RobotCard/RobotCard";
+import "./RobotsList.css";
 
 const RobotsList = (): React.ReactElement => {
   const robots = useAppSelector(({ robotsStore: { robots } }) => robots);
 
   return (
-    <ul className="robots-list">
+    <ul className="robots-page__list">
       {robots.map((robot) => (
         <li key={robot.id} className="robot-list__robot">
           <RobotCard robot={robot} />
